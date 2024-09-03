d0525af91c557d0d9738c313cc1f3326a351d3a1
JucaHF24 <juancarlos,fr6@gmail.com>
2024-09-03 10:16:31 -0600

    create education branch #3

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,11 +14,12 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'mycv' title`, () => {
+  it('should have the \'mycv\' title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('mycv');
   });
+
 
   xit('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
